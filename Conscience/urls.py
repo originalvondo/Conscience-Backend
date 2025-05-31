b65de57dfd79f52f38e45ceda8285379483e4cb9
Tanvirul Islam <94637393+originalvondo@users.noreply.github.com>
2025-06-01 01:05:05 +0600

    .

--- Conscience/urls.py
+++ Conscience/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path 
 from . import views 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('magazines/', views.getMagazines, name="magazines"),
     path('magazine/<slug:slug>/', views.getMagazineInfo, name="magazine"),
     path('author/<str:username>/', views.getAuthorInfo, name="author"),
     path('author/<str:username>/publications/', views.getAuthorPublications, name="authorPublications"),
-]
+] 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
